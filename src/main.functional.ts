1fbff51c311dead9303c91f933a22e5bf5a94d27
Srilekha Kirsh <skirsh@mozilla.com>
2022-05-03 11:32:04 -0700

    use import  for supertest request and remove docker from functional test - supertest directly hits the endpoints

--- src/main.functional.ts
+++ src/main.functional.ts
@@ -1,13 +1,8 @@
 import { expect } from 'chai';
 import { app } from './main';
+import request = require('supertest');
 
 describe('functional tests', () => {
-  /*eslint-disable */
-  //there are no types-supertest, so we use require.
-  //this throws lint error.
-  const request = require('supertest');
-  /*eslint-enable*/
-
   describe('health endpoint', () => {
     it('should return 200 OK', async () => {
       const res = await request(app).get('/.well-known/server-health');
