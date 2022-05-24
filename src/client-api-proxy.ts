c177d7f6b9357e9c7c10724d99042612b6290ba7
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2022-05-25 02:11:15 +1000

    feat(proxy): [BACK-1443] Add ability to test with Dev Client API (#35)

--- src/client-api-proxy.ts
+++ src/client-api-proxy.ts
@@ -9,7 +9,7 @@ import {
 } from './types';
 
 const client = new ApolloClient({
-  link: new HttpLink({ fetch, uri: 'https://client-api.getpocket.com' }),
+  link: new HttpLink({ fetch, uri: config.clientApi.uri }),
   cache: new InMemoryCache(),
   name: config.app.apolloClientName,
   version: config.app.version,
